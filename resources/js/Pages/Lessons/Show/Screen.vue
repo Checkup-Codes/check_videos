9eafc1382d7afabdb923de796fb636629d56b2d3
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2024-10-27 16:31:03 +0300

    fix : removed keyword field from lesson page

--- resources/js/Pages/Lessons/Show/Screen.vue
+++ resources/js/Pages/Lessons/Show/Screen.vue
@@ -78,27 +78,6 @@
           </button>
         </div>
       </div>
-
-      <!-- Keywords Section -->
-      <div class="mt-6 rounded-lg bg-white p-6 shadow-lg">
-        <h2 class="mb-4 text-2xl font-semibold text-gray-800">Keywords</h2>
-        <div>
-          <textarea
-            v-model="keywords"
-            class="h-40 w-full rounded border bg-gray-50 p-4 outline-none focus:ring-2 focus:ring-gray-500"
-            placeholder="Keywordleri virgülle ayrılmış şekilde girin"
-            rows="4"
-          ></textarea>
-          <div class="flex justify-end">
-            <button
-              @click="copyToClipboard"
-              class="mt-3 w-1/3 rounded bg-gray-800 py-2 text-white transition-all hover:bg-gray-700"
-            >
-              Kopyala
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -206,14 +185,5 @@ const parseDuration = (isoDuration) => {
   const minutes = matches[2] ? matches[2].slice(0, -1) : '00';
   const seconds = matches[3] ? matches[3].slice(0, -1) : '00';
   return `${hours}:${minutes}:${seconds}`;
-};
-
-const keywords = ref(
-  'yazılım öğrenme yolları, yazılım öğrenme siteleri, yazılım öğrenme platformları, yazılım öğrenme kitapları, yazılım öğrenme videoları, yazılım öğrenme uygulamaları'
-);
-
-const copyToClipboard = () => {
-  navigator.clipboard.writeText(keywords.value);
-  alert('Keywordler kopyalandı!');
 };
 </script>
