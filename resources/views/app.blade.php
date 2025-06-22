fe582c9aafdcbd511e7e23ec37f7c2ac91cafced
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2025-06-22 12:35:26 +0300

    fix : favicon for multi tenancy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Geist+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
+    <link rel="icon" href="{{ $page['props']['screen']['seo']['logo'] ?? '/favicon.ico' }}" />
+
     <!-- Theme initialization script -->
     <script>
         // Mevcut tema ayarını localStorage'dan al veya varsayılan olarak 'light' kullan
