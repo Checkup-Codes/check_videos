8465d299633cf432c9da15fc0eb2e4cac1ef722b
Yakup Sarı <61888196+cekapykp@users.noreply.github.com>
2025-11-30 14:31:30 +0300

    v0.7.6 : restored layout

--- public/build/assets/CreateBookmarks-CXu47pWq.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-C42vRrUM.js";/* empty css            */import"./ssr-B6-2V9vM.js";const e={};function t(o,c){return"Create"}const s=r(e,[["render",t]]);export{s as default};
--- public/build/assets/EditBookmarks-CFfY_Pve.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-C42vRrUM.js";/* empty css            */import"./ssr-B6-2V9vM.js";const t={};function o(c,e){return"ada"}const s=r(t,[["render",o]]);export{s as default};
